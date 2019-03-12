13f0326c407587dfb54633b576457ef8fbaee43a
palupianindya <palupisuyeta84@gmail.com>
2019-03-12 10:12:00 +0700

    Latihan Konstanta

---
+++ latihanconstanta.php
@@ -0,0 +1,12 @@
+<?php
+
+define ('VERSION', '1.0.0');
+
+const SITE_NAME ="MABES TNI";
+const BASE_URL = "https://www.tni.mil.id/";
+
+echo "Site name: " .SITE_NAME. "</br>";
+echo "base url: " .BASE_URL. "</br>";
+echo "Version: " .VERSION. "</br>";
+
+?>
